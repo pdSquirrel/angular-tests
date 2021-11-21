cb41072d8b861a77278940a931b22c099dd0e249
Roger Nordqvist <progdev.squirrel@gmail.com>
2021-11-21 10:21:36 +0100

    template finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'tests';
+  title = 'Password Generator';
+  length = 0;
+  useLetters = false;
+  useNumbers = false;
+  useSymbols = false;
+  password = '';
+
+  onChangeLength(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.useLetters = !this.useLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.useSymbols = !this.useSymbols;
+  }
+
+  buttonClicked() {
+    console.log(`
+    Settings:
+    Use letters: ${this.useLetters}
+    Use numbers: ${this.useNumbers}
+    Use symbols: ${this.useSymbols}
+    `);
+  }
 }
